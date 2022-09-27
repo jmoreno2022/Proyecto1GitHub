3c8d19ead8ebd430b051653875984a9d8f9ccdbe
jmoreno <j.moreno36@alumnos.santotomas.cl>
2022-09-27 10:30:15 -0300

    creacion del proyecto parte 2

--- app/src/main/java/com/jmoreno/proyecto1github/MainActivity.java
+++ app/src/main/java/com/jmoreno/proyecto1github/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //comentario 2
+        //comentario 22
     }
 }
